904ef5438884576194f3ecc0389bcae578767b57
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-02-04 16:59:44 +0900

    [PP-4] WebGL을 통해 리듬게임 표시, 이전 JS 구현 삭제

--- src/utils/setCanvasDPR.ts
+++
@@ -1,9 +0,0 @@
-// 메모리에 실제 크기 설정 (픽셀 밀도를 고려하여 크기 조정)
-function setCanvasDPR(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, dpr: number) {
-  canvas.width *= dpr;
-  canvas.height *= dpr;
-
-  ctx.scale(dpr, dpr);
-}
-
-export default setCanvasDPR;
