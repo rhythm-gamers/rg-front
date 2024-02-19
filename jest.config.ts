20c1d19a4032ff38803f135a8613a5e974f95a7c
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-02-19 23:21:11 +0900

    [CONFIG-11] eslint, jest, tsconfig 설정 추가

--- jest.config.ts
+++ jest.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
