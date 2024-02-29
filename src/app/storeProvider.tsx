bdc2110df0789d942594d6bb58f755a837e250df
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-03-01 01:59:38 +0900

    [RDX-1] redux-logger 추가 및 store 연결 방식 변경

--- src/app/storeProvider.tsx
+++ src/app/storeProvider.tsx
@@ -2,18 +2,24 @@
 
 import { ReactNode, useRef } from "react";
 import { Provider } from "react-redux";
-import { IPersistStore, makeStore } from "../lib/store";
+import { makeStore, AppStore } from "../lib/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { Persistor, persistStore } from "redux-persist";
 
 interface IStoreProvider {
   children: ReactNode;
 }
 
+interface IEnhancedAppStore extends AppStore {
+  __persistor?: Persistor;
+}
+
 export default function StoreProvider({ children }: IStoreProvider) {
-  const storeRef = useRef<IPersistStore | null>(null);
+  const storeRef = useRef<IEnhancedAppStore>();
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore();
+    storeRef.current.__persistor = persistStore(storeRef.current);
   }
   return (
     <Provider store={storeRef.current}>
