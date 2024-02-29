c7c59f3004c86b6924c51bb7c65f52610425e253
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-03-01 02:11:40 +0900

    [RDX-3] 패턴 연습 검색 옵션에 관한 Slice 추가

--- src/lib/rootReducer.ts
+++ src/lib/rootReducer.ts
@@ -1,9 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import counter from "./features/counter/counterSlice";
 import unity from "./features/unity/unitySlice";
+import practice from "./features/practice/practiceSlice";
 
 // 여러 개의 reducer들을 하나로 합쳐주는 함수
 export default combineReducers({
-  counter,
   unity,
+  practice,
 });
