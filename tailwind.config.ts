f8e861b97154b5a9bf7c64cdcd14c200905ba308
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-03-09 18:31:26 +0900

    Tailwind: level-test 드래그 안내 애니메이션 3개 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       animation: {
         leftZoom: "leftZoom 4s ease-in-out infinite",
         rightZoom: "rightZoom 4s ease-in-out infinite",
+        clickAndDrag: "clickAndDrag 6s ease-in-out infinite",
+        leftDrag: "leftDrag 3s ease-in-out infinite",
+        rightDrag: "rightDrag 3s ease-in-out infinite",
       },
       keyframes: {
         leftZoom: {
@@ -20,6 +23,21 @@ const config: Config = {
         rightZoom: {
           "0, 40%": { transform: "translate(0, 0)" },
           "30%": { transform: "translate(-50%, 0)" },
+        },
+        clickAndDrag: {
+          "10%": { transform: "scale(0.8)" },
+          "25%": { transform: "scale(0.8) translate(80%, 0)" },
+          "35%": { transform: "scale(0.8) translate(-80%, 0)" },
+          "50%": { transform: "scale(1) translate(0, 0)" },
+          "100%": { transform: "scale(1) translate(0, 0)" },
+        },
+        leftDrag: {
+          "0%": { transform: "translate(0, 0)" },
+          "25%": { transform: "translate(-80%, 0)" },
+        },
+        rightDrag: {
+          "0%": { transform: "translate(0, 0)" },
+          "25%": { transform: "translate(80%, 0)" },
         },
       },
     },
