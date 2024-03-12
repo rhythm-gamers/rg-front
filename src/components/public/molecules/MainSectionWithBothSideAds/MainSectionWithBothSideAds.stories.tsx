87ca2d5cb7a59fd28f9089375afaf3d0a064c371
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-03-12 20:18:26 +0900

    Storybook: MainSection과 그 양 옆에 광고가 있는 MainSectionWithBothSideAds 추가

---
+++ src/components/public/molecules/MainSectionWithBothSideAds/MainSectionWithBothSideAds.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import MainSectionWithBothSideAds from "./MainSectionWithBothSideAds";
+
+const meta = {
+  title: "MainSectionWithBothSideAds",
+  component: MainSectionWithBothSideAds,
+  args: { sectionTitle: "섹션 제목", children: "children" },
+} satisfies Meta<typeof MainSectionWithBothSideAds>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
