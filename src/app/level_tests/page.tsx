bbc9105961e9070d580a1044e598bd0850e48ca4
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-03-12 20:20:14 +0900

    Design: 컴포넌트가 Slider 부분만 포함하도록 변경

--- src/app/level_tests/page.tsx
+++ src/app/level_tests/page.tsx
@@ -1,9 +1,30 @@
 import LevelCards from "@/components/level_tests/organisms/LevelCards/LevelCards";
+import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
+import { TbHandClick } from "react-icons/tb";
 
 const RhythmLevelTests = () => {
   return (
     <main className="flex flex-col items-center w-full h-[calc(100vh-3.75rem)] bg-orange-100">
-      <LevelCards />
+      <div className="flex w-full h-full justify-evenly items-center relative">
+        <LevelCards />
+        <div className="flex flex-col items-center gap-3 absolute bottom-8 mx-auto text-rose-400">
+          <div className="flex justify-between w-[35rem]">
+            <div className="flex items-center animate-leftDrag">
+              <MdArrowBackIos className="text-rose-400" size={30} />
+              <MdArrowBackIos className="text-rose-300" size={30} />
+              <MdArrowBackIos className="text-rose-200" size={30} />
+            </div>
+            <TbHandClick className="animate-clickAndDrag" size={55} />
+            <div className="flex items-center animate-rightDrag">
+              <MdArrowForwardIos className="text-rose-200" size={30} />
+              <MdArrowForwardIos className="text-rose-300" size={30} />
+              <MdArrowForwardIos className="text-rose-400" size={30} />
+            </div>
+          </div>
+          <p className="text-lg">좌우로 스크롤하세요!</p>
+          <p className="text-black">Tip: 방향키로도 이동 가능해요.</p>
+        </div>
+      </div>
     </main>
   );
 };
