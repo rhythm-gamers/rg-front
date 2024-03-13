3ae113b2218ce8730ad01d7b267aa0c892fab8fe
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-03-13 19:06:38 +0900

    Design: height가 길 때도 광고가 중앙에 위치할 수 있도록 css 수정

--- src/components/public/atoms/AdSideBox/AdSideBox.tsx
+++ src/components/public/atoms/AdSideBox/AdSideBox.tsx
@@ -1,7 +1,7 @@
 const AdSideBox = () => {
   return (
-    <aside className="w-[200px] h-screen mx-16 flex items-center">
-      <div className="w-full h-[60vh] bg-gray-300"></div>
+    <aside className="min-w-[130px] mx-16">
+      <div className="w-full h-[60vh] sticky top-44 bg-gray-300"></div>
     </aside>
   );
 };
