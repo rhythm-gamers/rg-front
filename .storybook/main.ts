3c36b62b7a0076d216b23aa1892cb0e172819270
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-03-18 17:21:00 +0900

    Fix: test 환경에 env가 적용이 안되는 문제 해결 중 2

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -53,6 +53,11 @@ const config: StorybookConfig = {
     name: "@storybook/nextjs",
     options: {},
   },
+  env: (config) => ({
+    ...config,
+    AWS_CLOUDFRONT_HOSTNAME: process.env.AWS_CLOUDFRONT_HOSTNAME!,
+    NEXT_PUBLIC_AWS_CLOUDFRONT_URL: process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL!,
+  }),
   docs: {
     autodocs: "tag",
   },
