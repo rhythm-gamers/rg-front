26dcea8b52e6014049f15803b42fcde01fb3d6f1
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-03-18 18:11:05 +0900

    Fix: test 환경에 env가 적용이 안되는 문제 해결 중 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,36 @@
 /** @type {import('next').NextConfig} */
+const injectVars = Object.keys(process.env).reduce((c, key) => {
+  if (/^NEXT_PUBLIC_/.test(key)) {
+    c[`process.env.${key}`] = JSON.stringify(process.env[key]);
+  }
+  return c;
+}, {});
+
+function injectEnv(definitions) {
+  const env = "process.env";
+
+  if (!definitions[env]) {
+    return {
+      ...definitions,
+      [env]: JSON.stringify(
+        Object.fromEntries(
+          Object.entries(definitions)
+            .filter(([key]) => key.startsWith(env))
+            .map(([key, value]) => [
+              key.substring(env.length + 1),
+              JSON.parse(value),
+            ]),
+        ),
+      ),
+    };
+  }
+  return definitions;
+}
+
 const nextConfig = {
+  core: {
+    builder: "webpack5",
+  },
   headers: async () => [
     {
       source: "/:path*",
@@ -17,7 +48,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.AWS_CLOUDFRONT_HOSTNAME,
+        hostname: process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_HOSTNAME,
         port: "",
         pathname: "/**",
       },
@@ -30,6 +61,25 @@ const nextConfig = {
         destination: `${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL}/Sheet/:path*`,
       },
     ];
+  },
+  webpackFinal: (config) => {
+    config.plugins = config.plugins.reduce((c, plugin) => {
+      if (plugin instanceof webpack.DefinePlugin) {
+        return [
+          ...c,
+          new webpack.DefinePlugin(
+            injectEnv({
+              ...plugin.definitions,
+              ...injectVars,
+            }),
+          ),
+        ];
+      }
+
+      return [...c, plugin];
+    }, []);
+
+    return config;
   },
   output: "standalone",
 };
