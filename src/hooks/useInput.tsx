84674f05fb8a7970899d003d5c6371d388b0b835
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-04-02 14:25:53 +0900

    Refactor: input type별로 동작을 정의하여 재사용성을 높임

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,19 +1,57 @@
-import { ChangeEvent, RefObject, useRef, useState } from "react";
+import { notIncludeSpace, notIncludeSpecialChar } from "@/const";
+import { ChangeEvent, useState } from "react";
 
-const useInput = (
-  initText?: string,
-): [
-  string,
-  (e: ChangeEvent<HTMLInputElement>) => void,
-  RefObject<HTMLInputElement>,
-] => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [value, setValue] = useState(initText || "");
+type TNotIncludes = "space" | "specialChar";
+
+interface IUseInput<T> {
+  type: "text" | "checkbox" | "file";
+  notIncludes?: TNotIncludes[];
+  initValue: T;
+}
+
+function useInput<T>({
+  type,
+  notIncludes,
+  initValue,
+}: IUseInput<T>): [T, (e: ChangeEvent<HTMLInputElement>) => void] {
+  const [value, setValue] = useState<T>(initValue);
   const changeInputValue = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
+    switch (type) {
+      case "text": {
+        let currValue = e.target.value;
+        notIncludes?.forEach((patternName) => {
+          patternName === "space"
+            ? (currValue = currValue.replace(notIncludeSpace, ""))
+            : patternName === "specialChar"
+            ? (currValue = currValue.replace(notIncludeSpecialChar, ""))
+            : null;
+        });
+        setValue(currValue as T);
+        break;
+      }
+      case "checkbox": {
+        setValue((prev) => !prev as T);
+        break;
+      }
+      case "file": {
+        const fileBlob = e.target.files?.[0];
+        if (!fileBlob) return;
+
+        const reader = new FileReader();
+        reader.readAsDataURL(fileBlob);
+        return new Promise<void>((resolve) => {
+          reader.onload = () => {
+            if (typeof reader.result === "string") {
+              setValue(reader.result as T);
+            }
+            resolve();
+          };
+        });
+      }
+    }
   };
 
-  return [value, changeInputValue, inputRef];
-};
+  return [value, changeInputValue];
+}
 
 export default useInput;
