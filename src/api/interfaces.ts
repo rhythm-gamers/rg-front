0b73dcbb22d7b0771f6660a158c5e3df5210e581
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-04-11 21:20:07 +0900

    Feat: 패턴 정보 인터페이스 추가

---
+++ src/api/interfaces.ts
@@ -0,0 +1,11 @@
+// 여러 API 모듈들에서 사용되는 공통 인터페이스들을 작성
+
+export interface IPatternInfo {
+  roll: number;
+  offGrid: number;
+  stairs: number;
+  peak: number;
+  multiples: number;
+  trill: number;
+  hold: number;
+}
