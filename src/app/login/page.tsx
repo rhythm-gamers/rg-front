b4021361847fbc82c096fe031477e62ff956c64d
ZeppIine <kim0904min@gmail.com>
2024-07-01 15:49:22 +0900

    Design: 로그인 form 디자인 변경

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -34,22 +34,26 @@ const Login = () => {
     })
   }
 
-  return <FullScreenSection direction={"row"} className="flex justify-center items-center">
-    <form className="flex flex-col justify-center items-center border rounded-3xl border-[#39c5bb] p-10 gap-4" onSubmit={login}>
-      <h3 className="text-3xl mb-4">Login</h3>
-      <input type="text" value={userEmail} placeholder="Email" onChange={changeUserEmail}/>
-      <input type="password" value={userPassword} placeholder="Password" onChange={changeUserPassword}/>
-      <div className="flex justify-between">
-        <Checkbox value={"Remember me"} checked={isRemember} onChange={changeIsRemember}/>
-        <Link href={"/"}>Forgot Password?</Link>
+  return (
+    <div className="w-full h-full bg-gradient-to-r from-rose-400 to-orange-200 relative">
+  <FullScreenSection direction={"row"} className="text-white flex justify-center items-center">
+    <form className="w-1/5 backdrop-blur-2xl flex flex-col justify-center items-center border-2 rounded-3xl border-[#F6FDC4] p-10 gap-4" onSubmit={login}>
+      <h3 className="text-3xl mb-4">로그인</h3>
+      <input className="bg-transparent border-2 rounded-3xl border-[#F6FDC4] w-full px-4 py-2 placeholder-white" type="text" value={userEmail} placeholder="Email" onChange={changeUserEmail}/>
+      <input className="bg-transparent border-2 rounded-3xl border-[#F6FDC4] w-full px-4 py-2 placeholder-white" type="password" value={userPassword} placeholder="Password" onChange={changeUserPassword}/>
+      <div className="w-full flex justify-between">
+        <Checkbox value={"아이디 저장"} checked={isRemember} onChange={changeIsRemember}/>
+        <Link href={"/"}>비밀번호 찾기</Link>
       </div>
-      <button className="border rounded-3xl border-[#39c5bb] w-full py-2">Login</button>
-      <div>
-        Don't have an account?
-        <Link href={"/"}> Register</Link>
+      <button className="border-2 rounded-3xl border-[#F6FDC4] bg-[#F6FDC4] w-full py-2 text-black">로그인</button>
+      <div className="w-full flex justify-center">
+        계정이 없으신가요?
+        <Link className="mx-4" href={"/"}>회원가입</Link>
       </div>
     </form>
-  </FullScreenSection>;
+  </FullScreenSection>
+  </div>
+  )
 };
 
 export default Login;
