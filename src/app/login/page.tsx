b03ff0a8bc972624e521401cf7128743ce274181
ZeppIine <kim0904min@gmail.com>
2024-07-01 20:57:23 +0900

    Design: 기존 색상코드 명칭 지정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -37,15 +37,15 @@ const Login = () => {
   return (
     <div className="w-full h-full bg-gradient-to-r from-rose-400 to-orange-200 relative">
   <FullScreenSection direction={"row"} className="text-white flex justify-center items-center">
-    <form className="w-1/5 backdrop-blur-2xl flex flex-col justify-center items-center border-2 rounded-3xl border-[#F6FDC4] p-10 gap-4" onSubmit={login}>
+    <form className="w-1/5 backdrop-blur-2xl flex flex-col justify-center items-center border-2 rounded-3xl border-bright-cream-1 p-10 gap-4" onSubmit={login}>
       <h3 className="text-3xl mb-4">로그인</h3>
-      <input className="bg-transparent border-2 rounded-3xl border-[#F6FDC4] w-full px-4 py-2 placeholder-white" type="text" value={userEmail} placeholder="Email" onChange={changeUserEmail}/>
-      <input className="bg-transparent border-2 rounded-3xl border-[#F6FDC4] w-full px-4 py-2 placeholder-white" type="password" value={userPassword} placeholder="Password" onChange={changeUserPassword}/>
+      <input className="bg-transparent border-2 rounded-3xl border-bright-cream-1 w-full px-4 py-2 placeholder-white" type="text" value={userEmail} placeholder="Email" onChange={changeUserEmail}/>
+      <input className="bg-transparent border-2 rounded-3xl border-bright-cream-1 w-full px-4 py-2 placeholder-white" type="password" value={userPassword} placeholder="Password" onChange={changeUserPassword}/>
       <div className="w-full flex justify-between">
         <Checkbox value={"아이디 저장"} checked={isRemember} onChange={changeIsRemember}/>
         <Link href={"/"}>비밀번호 찾기</Link>
       </div>
-      <button className="border-2 rounded-3xl border-[#F6FDC4] bg-[#F6FDC4] w-full py-2 text-black">로그인</button>
+      <button className="border-2 rounded-3xl border-bright-cream-1 bg-bright-cream-1 w-full py-2 text-black">로그인</button>
       <div className="w-full flex justify-center">
         계정이 없으신가요?
         <Link className="mx-4" href={"/"}>회원가입</Link>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
         "chingho-rank-2": colors.lime[300],
         "chingho-rank-3": colors.indigo[400],
         "chingho-rank-4": colors.red[400],
+        "bright-cream-1": '#F6FDC4',
       },
       animation: {
         leftZoom: "leftZoom 6s ease-in-out infinite",
