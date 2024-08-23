a123fe85647dcf772d1ac1c93082a1fe19fae75f
ZeppIine <kim0904min@gmail.com>
2024-08-23 22:17:45 +0900

    Fix: 엔드포인트에 맞춰서 인터페이스 수정

--- src/api/post/getOne.ts
+++ src/api/post/getOne.ts
@@ -1,4 +1,4 @@
-import { IPostRes } from ".";
+import { IGetOnePost } from ".";
 import fetchExtended from "../fetchExtended";
 import { parseJsonWithWrap } from "../utils";
 
@@ -7,8 +7,11 @@ import { parseJsonWithWrap } from "../utils";
  * @param id board id
  * @returns
  */
-const getOne = async (id: number): Promise<{ data: IPostRes }> => {
-  const res = await fetchExtended(`/post/spec/${id}`, { method: "get" });
+const getOne = async (id: number): Promise<{ data: IGetOnePost }> => {
+  const res = await fetchExtended(`/post/spec/${id}`, {
+    method: "get",
+    cache: "no-store",
+  });
   const json = await res.json();
   const parsedJson = parseJsonWithWrap(json);
   return parsedJson;
--- src/api/post/index.ts
+++ src/api/post/index.ts
@@ -14,7 +14,16 @@ export interface IPostReq {
 export interface IPostReqOptional
   extends Partial<Omit<IPostReq, "boardName">> {}
 
-export interface IPostRes {
+export interface IGetAllPosts {
+  posts: IPost[];
+  allCount: number;
+}
+
+export interface IGetOnePost {
+  post: IPost;
+}
+
+interface IPost {
   id: number;
   title: string;
   content: string;
