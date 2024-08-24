0a0536d7c793ce692f16d4a63e271da5ab41b711
ZeppIine <kim0904min@gmail.com>
2024-08-24 22:06:33 +0900

    Feat: 페이지 버튼 동적 생성 추가

--- src/app/(loginless)/community/page.tsx
+++ src/app/(loginless)/community/page.tsx
@@ -3,18 +3,50 @@ import MainSectionWithBothSideAds from "@/components/molecules/MainSectionWithBo
 import CustomBtn from "../../../components/atoms/CustomBtn/CustomBtn";
 import Link from "next/link";
 import PostAPI from "@/api/post";
+import PaginationBtn from "../../../components/molecules/PaginationBtn/PaginationBtn";
+import {
+  MdKeyboardArrowRight,
+  MdKeyboardDoubleArrowRight,
+  MdKeyboardArrowLeft,
+  MdKeyboardDoubleArrowLeft,
+} from "react-icons/md";
+
+const boardRowLimit = 1;
 
 const Community = async ({
   searchParams,
 }: {
   searchParams: { page?: number };
 }) => {
-  const page = searchParams.page ?? 0;
+  const page = searchParams.page ?? 1;
   const newPosts = await PostAPI.getAllByBoardName({
     boardName: "자유게시판",
-    page: page,
-    limit: 30,
+    page: page - 1,
+    limit: boardRowLimit,
   });
+  const pageCount = Math.ceil(newPosts.data.allCount / boardRowLimit);
+
+  const generatePages = (currentPage: number, totalPage: number): number[] => {
+    let start, end;
+
+    if (totalPage <= 10) {
+      start = 1;
+      end = totalPage;
+    } else if (currentPage <= 5) {
+      start = 1;
+      end = 10;
+    } else if (currentPage >= totalPage - 4) {
+      start = Math.max(totalPage - 9, 1);
+      end = totalPage;
+    } else {
+      start = currentPage - 4;
+      end = currentPage + 5;
+    }
+
+    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  };
+
+  const pages = generatePages(page, pageCount);
 
   return (
     <MainSectionWithBothSideAds sectionTitle="자유 게시판">
@@ -40,13 +72,19 @@ const Community = async ({
             commentCount={post.commentCount}
           />
         ))}
-
-        <div className="flex justify-end">
-          <Link href="/community/write">
-            <CustomBtn size={"sm"} type={"deny"} className="my-2">
-              글쓰기
-            </CustomBtn>
-          </Link>
+        <div>
+          <div className="flex justify-end">
+            <Link href="/community/write">
+              <CustomBtn size={"sm"} type={"deny"} className="my-2">
+                글쓰기
+              </CustomBtn>
+            </Link>
+          </div>
+          <div className="flex justify-center gap-1">
+            {pages.map((page) => (
+              <PaginationBtn pageNumber={page} />
+            ))}
+          </div>
         </div>
       </div>
     </MainSectionWithBothSideAds>
@@ -54,3 +92,15 @@ const Community = async ({
 };
 
 export default Community;
+
+{
+  /* <button className="flex h-5 w-5 justify-center items-center">
+              <MdKeyboardArrowRight size={20} className="text-rhythm-theme" />
+            </button>
+            <button className="flex h-5 w-5 justify-center items-center">
+              <MdKeyboardDoubleArrowRight
+                size={20}
+                className="text-rhythm-theme"
+              />
+            </button> */
+} //TODO: 애로우 버튼 생성 필요
