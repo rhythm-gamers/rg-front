013d1009c75b6fe49937f2dde69c59fd8ee78e09
ZeppIine <kim0904min@gmail.com>
2024-08-27 20:46:20 +0900

    Storybook: PaginationBtn 컴포넌트 스토리북 작성

---
+++ src/components/molecules/PaginationBtn/PaginationBtn.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import PaginationBtn from "./PaginationBtn";
+
+const meta = {
+  title: "PaginationBtn",
+  component: PaginationBtn,
+  args: {
+    pageNumber: 1,
+  },
+} satisfies Meta<typeof PaginationBtn>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const typeDefault: Story = {};
