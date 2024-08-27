e65d592a0ebc57f37a88ac271c810dae00ae7e44
ZeppIine <kim0904min@gmail.com>
2024-08-27 21:33:47 +0900

    Feat: 추천 버튼 추가 및 기능 구현 필요

--- src/app/(loginless)/community/[boardIndex]/page.tsx
+++ src/app/(loginless)/community/[boardIndex]/page.tsx
@@ -1,6 +1,8 @@
 import PostAPI from "@/api/post";
+import CustomBtn from "@/components/atoms/CustomBtn/CustomBtn";
 import SectionTitle from "@/components/atoms/SectionTitle/SectionTitle";
 import MainSectionWithBothSideAds from "@/components/molecules/MainSectionWithBothSideAds/MainSectionWithBothSideAds";
+import { FaRegThumbsUp } from "react-icons/fa";
 
 const Board = async ({ params }: { params: { boardIndex: number } }) => {
   const {
@@ -23,8 +25,6 @@ const Board = async ({ params }: { params: { boardIndex: number } }) => {
             <div>
               <span>조회수: {views}</span>
               {" | "}
-              <span>추천: {likes}</span>
-              {" | "}
               <span>{new Date(createdAt).toLocaleDateString()}</span>
             </div>
           </div>
@@ -32,6 +32,17 @@ const Board = async ({ params }: { params: { boardIndex: number } }) => {
         <hr className="mt-2 mb-5 border-black" />
         <div className="leading-relaxed">
           <p>{content}</p>
+        </div>
+        <div className="my-2 flex flex-col justify-center items-center">
+          <p>추천</p>
+          <CustomBtn
+            size={"md"}
+            type={"accept"}
+            className="mb-2 !px-8 flex gap-3 items-center"
+          >
+            <FaRegThumbsUp />
+            <span>{likes}</span>
+          </CustomBtn>
         </div>
       </div>
     </MainSectionWithBothSideAds>
