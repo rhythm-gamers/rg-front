49dfb07b894728f3d0f63af7c1699fae41986752
ZeppIine <kim0904min@gmail.com>
2024-10-06 19:54:44 +0900

    Feat: CommentWriteBox 컴포넌트 생성 및 스토리 추가 필요

---
+++ src/components/molecules/CommentWriteBox/CommentWriteBox.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { PiArrowElbowDownRightBold, PiPaperPlaneTilt } from "react-icons/pi";
+import UserProfile, { IUserProfile } from "../UserProfile/UserProfile";
+import { useRef } from "react";
+
+interface ICommentWriteBox extends IUserProfile {
+  isReComment: boolean;
+}
+
+const CommentWriteView = ({
+  isReComment,
+  imgSrc,
+  imgAlt,
+  nickname,
+  level,
+}: ICommentWriteBox) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const changeTextareaHeight = () => {
+    if (textareaRef.current == null) return;
+    textareaRef.current.style.height = "auto";
+    textareaRef.current.style.height = textareaRef.current.scrollHeight + "px";
+  };
+
+  return (
+    <div
+      className={`flex flex-col p-6 gap-3 bg-gray-200 rounded-2xl ${
+        isReComment && `w-full`
+      }`}
+    >
+      <UserProfile
+        imgSrc={imgSrc}
+        imgAlt={imgAlt}
+        nickname={nickname}
+        level={level}
+      />
+      <div className="flex flex-row items-end justify-between w-full p-4 gap-2 bg-white rounded-xl">
+        <textarea
+          ref={textareaRef}
+          className="w-full h-auto resize-none outline-none"
+          rows={1}
+          onChange={changeTextareaHeight}
+          maxLength={8000}
+          placeholder="댓글을 입력해주세요"
+        />
+        <button
+          onClick={() => {
+            console.log(textareaRef.current?.value);
+          }}
+        >
+          <PiPaperPlaneTilt className="text-2xl" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const CommentWriteBox = ({
+  isReComment,
+  imgSrc,
+  imgAlt,
+  nickname,
+  level,
+}: ICommentWriteBox) => {
+  if (isReComment)
+    return (
+      <div className="flex flex-row my-2">
+        <PiArrowElbowDownRightBold className="text-2xl mx-2 mt-2" />
+        <CommentWriteView
+          isReComment={isReComment}
+          imgSrc={imgSrc}
+          imgAlt={imgAlt}
+          nickname={nickname}
+          level={level}
+        />
+      </div>
+    );
+  else
+    return (
+      <CommentWriteView
+        isReComment={isReComment}
+        imgSrc={imgSrc}
+        imgAlt={imgAlt}
+        nickname={nickname}
+        level={level}
+      />
+    );
+};
+
+export default CommentWriteBox;
