4ff5b6012099697dda5c78bb3eb7d6cd6e1f0c70
ZeppIine <kim0904min@gmail.com>
2024-10-06 19:58:02 +0900

    Design: 글쓰기 페이지 디자인 수정

--- src/app/(loginless)/community/write/page.tsx
+++ src/app/(loginless)/community/write/page.tsx
@@ -3,6 +3,9 @@
 import "react-quill/dist/quill.snow.css";
 import dynamic from "next/dynamic";
 import MainSectionWithBothSideAds from "@/components/molecules/MainSectionWithBothSideAds/MainSectionWithBothSideAds";
+import Footer from "@/components/organisms/Footer/Footer";
+import CustomBtn from "@/components/atoms/CustomBtn/CustomBtn";
+import Link from "next/link";
 
 const BoardWrite = () => {
   const QuillNoSSRWrapper = dynamic(() => import("react-quill"), {
@@ -12,17 +15,10 @@ const BoardWrite = () => {
 
   const modules = {
     toolbar: [
-      [{ header: "1" }, { header: "2" }, { font: [] }],
-      [{ size: [] }],
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [
-        { list: "ordered" },
-        { list: "bullet" },
-        { indent: "-1" },
-        { indent: "+1" },
-      ],
+      [{ header: "1" }],
+      [{ font: [] }, { size: [] }],
+      ["bold", "italic", "underline", "strike"],
       ["link", "image", "video"],
-      ["clean"],
     ],
     clipboard: {
       matchVisual: false,
@@ -37,38 +33,50 @@ const BoardWrite = () => {
     "italic",
     "underline",
     "strike",
-    "blockquote",
-    "list",
-    "bullet",
-    "indent",
     "link",
     "image",
     "video",
   ];
 
   return (
-    <MainSectionWithBothSideAds sectionTitle="자유 게시판">
-      <div className="mx-auto w-[900px]">
-        <div>
-          <div className="flex justify-between">
-            <div>
-              <span>작성자</span>
-            </div>
-            <div>
-              <span>조회수: 0</span>
-              {" | "}
-              <span>추천: 0</span>
-              {" | "}
-              <span>2024-08-22</span>
+    <>
+      <MainSectionWithBothSideAds sectionTitle="자유 게시판">
+        <div className="mx-auto w-[900px]">
+          <div>
+            <div className="flex justify-between">
+              <div className="w-full">
+                <span>작성자</span>
+                <div>
+                  <textarea
+                    rows={1}
+                    className="w-full mt-2 p-3 resize-none border border-gray-300"
+                    placeholder="제목을 입력해주세요"
+                  />
+                </div>
+              </div>
             </div>
           </div>
+          <hr className="mt-2 mb-5 border-black" />
+          <div className="leading-relaxed h-[500px]">
+            <QuillNoSSRWrapper
+              modules={modules}
+              formats={formats}
+              theme="snow"
+              className="h-full"
+              placeholder="내용을 입력해주세요"
+            />
+          </div>
+          <div className="flex justify-end mt-14">
+            <Link href="/community">
+              <CustomBtn size={"sm"} type={"deny"} className="my-2">
+                글쓰기
+              </CustomBtn>
+            </Link>
+          </div>
         </div>
-        <hr className="mt-2 mb-5 border-black" />
-        <div className="leading-relaxed">
-          <QuillNoSSRWrapper modules={modules} formats={formats} theme="snow" />
-        </div>
-      </div>
-    </MainSectionWithBothSideAds>
+      </MainSectionWithBothSideAds>
+      <Footer />
+    </>
   );
 };
 
