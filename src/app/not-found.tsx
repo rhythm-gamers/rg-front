cd9e514fb27ea62ce93995579c8f12a5abf3e465
YongjaeHyun <dltjrrbs2020@gmail.com>
2024-10-08 06:36:58 +0000

    Feat: notFound 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const NotFound: NextPage = () => {
+  return (
+    <>
+      <div>404 - 페이지를 찾을 수 없습니다</div>
+    </>
+  );
+};
+
+export default NotFound;
