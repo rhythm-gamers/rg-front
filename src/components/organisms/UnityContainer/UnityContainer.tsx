c99f0b1ef2805f2421ef169fe9b16cf5b4841e28
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-10-09 19:01:57 +0900

    Feat: 유니티 게임 소리 조절 기능 추가

--- src/components/organisms/UnityContainer/UnityContainer.tsx
+++ src/components/organisms/UnityContainer/UnityContainer.tsx
@@ -2,11 +2,23 @@
 
 import LevelTestAPI from "@/api/level_tests";
 import PatternPracticeAPI from "@/api/pattern_practice";
-import { setJudgeOffset, setSpeed } from "@/lib/features/unity/unitySlice";
+import {
+  setJudgeOffset,
+  setSpeed,
+  setVolume,
+} from "@/lib/features/unity/unitySlice";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { useRouter } from "next/navigation";
 import { useRouteChangeEvents } from "nextjs-router-events";
-import { useCallback, useEffect, useRef, useState } from "react";
+import {
+  ChangeEvent,
+  MouseEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { MdVolumeDown, MdVolumeOff, MdVolumeUp } from "react-icons/md";
 import { SlSizeFullscreen } from "react-icons/sl";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import { ReactUnityEventParameter } from "react-unity-webgl/distribution/types/react-unity-event-parameters";
@@ -38,8 +50,47 @@ const UnityContainer = ({ id, referer }: IUnityContainer) => {
 
   const unloadRef = useRef<() => void>();
 
-  const { speed, judgeOffset, fourKeyMaps, fiveKeyMaps, sixKeyMaps } =
+  const { speed, judgeOffset, volume, fourKeyMaps, fiveKeyMaps, sixKeyMaps } =
     useAppSelector((state) => state.unity);
+
+  const [tempVolume, setTempVolume] = useState(volume.current);
+  const changeTempVolume = (e: ChangeEvent<HTMLInputElement>) => {
+    const parsedValue = parseFloat(e.currentTarget.value);
+    setTempVolume(parsedValue);
+    sendMessage("AudioManager", "WebGLSetVolume", parsedValue);
+  };
+  const mouseUpTempVolume = (e: MouseEvent<HTMLInputElement>) => {
+    const parsedValue = parseFloat(e.currentTarget.value);
+    setTempVolume(parsedValue);
+    dispatch(
+      setVolume({
+        current: parsedValue,
+      }),
+    );
+  };
+
+  const muteVolume = () => {
+    sendMessage("AudioManager", "WebGLSetVolume", 0);
+    setTempVolume(0);
+    dispatch(
+      setVolume({
+        current: 0,
+        prevMute: volume.current,
+      }),
+    );
+  };
+  const unMuteVolume = () => {
+    if (volume.prevMute) {
+      sendMessage("AudioManager", "WebGLSetVolume", volume.prevMute);
+      setTempVolume(volume.prevMute);
+      dispatch(
+        setVolume({
+          current: volume.prevMute,
+          prevMute: undefined,
+        }),
+      );
+    }
+  };
 
   const enableFullScreen = () => {
     requestFullscreen(true);
@@ -159,18 +210,41 @@ const UnityContainer = ({ id, referer }: IUnityContainer) => {
   /** END Use Device Pixel Ratio */
 
   return (
-    <div className="w-2/3 relative">
+    <div className="w-2/3 max-w-[1280px] relative">
       {referer + " - " + id}
       <Unity
         unityProvider={unityProvider}
         devicePixelRatio={devicePixelRatio}
-        className={`w-full ${isLoaded ? "block" : "hidden"}`}
+        className={`w-full`}
       />
       <SlSizeFullscreen
         onClick={enableFullScreen}
         className="absolute right-6 bottom-6 cursor-pointer text-white"
         size={30}
       />
+      <div className="absolute left-6 bottom-6 flex justify-between items-center w-32">
+        {tempVolume === 0 ? (
+          <MdVolumeOff
+            size={28}
+            className="text-white"
+            onClick={unMuteVolume}
+          />
+        ) : tempVolume < 0.5 ? (
+          <MdVolumeDown size={28} className="text-white" onClick={muteVolume} />
+        ) : (
+          <MdVolumeUp size={28} className="text-white" onClick={muteVolume} />
+        )}
+        <input
+          type="range"
+          min={0}
+          max={1}
+          step={0.01}
+          value={tempVolume}
+          onChange={changeTempVolume}
+          onMouseUp={mouseUpTempVolume}
+          className="w-20"
+        />
+      </div>
     </div>
   );
 };
